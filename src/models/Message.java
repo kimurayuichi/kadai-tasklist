f69671800815b8237f86ee00200eec6a2a50164a
kimurayuichi <kimurayuichi.kanazawa@gmail.com>
2018-10-17 23:13:54 +0900

    Add getAllMessages

--- src/models/Message.java
+++ src/models/Message.java
@@ -5,9 +5,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllMessages",
+            query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+            )
+})
 @Table(name = "tasks")
 public class Message {
     @Id
@@ -15,7 +23,7 @@ public class Message {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "content")
+    @Column(name = "content", length = 225, nullable = false)
     private String content;
 
     public Integer getId(){
@@ -30,7 +38,7 @@ public class Message {
         return content;
     }
 
-    public void setContent(){
+    public void setContent(String content){
         this.content = content;
     }
 
